baf6d23ae4a65d0756ef201c3437e70f192e167d
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-04-04 22:07:33 +0200

    carousell finished

--- banner/js/main.js
+++ banner/js/main.js
@@ -4,14 +4,17 @@ var Premium = Premium || {};
 Premium.creative = {
 	init: function() {
 		/* START OF CUSTOM JS */
-
+		let image = document.querySelectorAll('.image');
 		let showDefault = function(){
-			// if(window.innerWidth >)
+			
 			
 		}
 
 		let showExpanded = function(){
-
+			if(window.innerWidth > 1024) {
+				image.style.width = '200px';
+				image.style.height = 160 + 'px';
+			}
 		}
 
 		Premium.jpxApi.on("default", showDefault);
