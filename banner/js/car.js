cc7321717b4affe0caf01ee467c2ae24c124be84
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-04-07 18:33:08 +0200

    animation improved

--- banner/js/car.js
+++ banner/js/car.js
@@ -1,11 +1,23 @@
 const car = document.querySelector(".car");
 const container = document.querySelector(".section-car");
 
+// Animation
 container.addEventListener("mousemove", (e) => {
     let xAxis = (window.innerWidth / 2 - e.pageX) / 5;
     let yAxis = (window.innerHeight / 2 - e.pageY) / 5;
 
     car.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
 }
-
 )
+
+// Animate In 
+container.addEventListener("mouseenter", (e) => {
+    car.style.transform = "none";
+});
+
+// Animate Out 
+
+container.addEventListener("mouseleave", (e) => {
+    car.style.transform = "all 1s ease";
+    car.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
